4607ef8ad3acc161de5d66045f5a9b99632107f0
keith-blanchard-projekt202 <keith.blanchard@projekt202.com>
2019-03-25 22:08:29 -0500

    Adding initial game files.

---
+++ public/index.js
@@ -0,0 +1,43 @@
+(function () {
+
+
+
+    const circles = [
+        {
+            x: 0,
+            y: 1,
+            radius: 10
+        },
+        {
+            x: 0,
+            y: 1,
+            radius: 100
+        },
+        {
+            x: 0,
+            y: 1,
+            radius: 50
+        }
+    ];
+
+
+    // incert circles
+
+    // give the index to the
+
+    // create random x value
+    // check for edge cases
+
+
+    // setInterval
+
+    // for all circles set x to + 10
+    // check for edge cases
+
+
+
+
+    // on click
+
+
+})();
