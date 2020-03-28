ad1d2268f361a966a38cd38c87c91ae5d547a109
Keith Blanchard <kblanchard111@gmail.com>
2020-03-28 04:51:21 -0500

    Fixing responsive issues.

--- public/src/Canvas.js
+++ public/src/Canvas.js
@@ -1,13 +1,12 @@
 export default class {
   init() {
     const $canvas = document.getElementById('canvas');
-    const $uiWrapper = document.getElementById('canvas');
-    let innerWidth = window.innerWidth;
-    if (innerWidth > 1021) {
-      innerWidth = 1024;
-    }
-    const width = innerWidth;
-    const height = window.innerHeight - $uiWrapper.height;
+    const $uiWrapper = document.getElementById('ui-wrapper');
+    const $uiWrapperHeight = window.getComputedStyle($uiWrapper).height;
+
+    const width = window.innerWidth;
+    const canvasBorderOffset = 80;
+    const height = window.innerHeight - parseFloat($uiWrapperHeight) - canvasBorderOffset;
     $canvas.setAttribute("width", width.toString());
     $canvas.setAttribute("height", height.toString());
     this.height = $canvas.height;
