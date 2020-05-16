cac66f5a1a2f0b5d01bc178169dd03d49f2dba01
Keith Blanchard <kblanchard111@gmail.com>
2020-05-15 23:41:32 -0500

    Fixing deploy script.

--- scripts/deploy-game.sh
+++ scripts/deploy-game.sh
@@ -1,24 +1,13 @@
 #!/bin/zsh
-mkdir ~/src/the-virus-game-deploy
-cd ~/src/the-virus-game-deploy
-rm -fr .git
+rm -fr ~/src/the-virus-game-deploy
 
-mkdir dist
+cp -r ~/src/bubble-game/dist/ ~/src/the-virus-game-deploy
+cd ~/src/the-virus-game-deploy/
 
-
-function copyFiles () {
-cp ~/src/austin-voice-coach/src/under-construction/index.html .
-cp ~/src/austin-voice-coach/src/CNAME .
-}
-
-function addFiles () {
-  git add index.html
-}
-
+cp ~/src/bubble-game/src/CNAME .
 
 git init
 git remote add origin git@github.com:keithblanchard/the-virus-game-deploy.git
-
-
+git add .
 git commit -m 'Deploying The Virus Game'
 git push -f -u origin master
